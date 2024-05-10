15793b1e5286abbcef045017b431d84bfe7cc5d6
Carlos <sotocarlos1956@gmail.com>
2024-05-10 12:05:46 -0400

    2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
 
-numofa = len(sys.argv) - 1
+if __name__ == "__main__":
+    numofa = len(sys.argv) - 1
 
-if numofa == 0:
-    print("NUmber of argument(s): {}".format(numofa))
-else:
-    print("Number of arguments(s): {}".format(numofa))
+    if numofa == 0:
+        print("NUmber of argument(s): {}".format(numofa))
+    else:
+        print("Number of arguments(s): {}".format(numofa))
 
-    print("Arguments:")
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("Arguments:")
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
