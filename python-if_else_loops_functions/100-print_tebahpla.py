a6d3bdb451046d1fb5864f81c2535148eb8a4b1d
Carlos <sotocarlos1956@gmail.com>
2024-05-11 12:14:57 -0400

    100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+aa = ""
 for letters in range(ord('z'), ord('a')-1, -1):
-        if letters % 2 == 0:
-            print(chr(letters), end="")
-        else:
-            print(chr(letters).upper(), end="")
+    if letters % 2 == 0:
+        aa += chr(letters)
+    else:
+        aa += chr(letters).upper()
+print(aa, end="")
