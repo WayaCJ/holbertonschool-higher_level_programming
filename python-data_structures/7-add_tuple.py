3e1392d081d612959aa6064933bd710b5d27e44f
Carlos <sotocarlos1956@gmail.com>
2024-05-14 12:00:57 -0400

    7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def add_tuple(tuple_a=(), tuple_b=()):
-
-        tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
-        tuple_b = tuple_b + (0, 0)[:2 - len(tuple_b)]
-
-        sum_first = tuple_a[0] + tuple_b[0]
-        sum_second = tuple_a[1] + tuple_b[1]
-    
-        return (sum_first, sum_second)
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return new_tuple
