957dab6d65a32dbfa285baa65e29127c71db5776
Carlos <sotocarlos1956@gmail.com>
2024-05-14 12:08:27 -0400

    deleting __name__

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     def multiple_returns(sentence):
-        if len(sentence) == 0:
-            return (0, None)
+        if len(sentence) == "":
+            return len(sentence), sentence[0]
         else:
-            return (len(sentence), sentence[0])
+            return len(sentence), None
