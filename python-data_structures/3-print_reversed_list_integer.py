c0f99d9bd53c891fd80a92564d3fd3450e02a073
Carlos <sotocarlos1956@gmail.com>
2024-05-14 11:44:36 -0400

    deleting __name__

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_reversed_list_integer(my_list=[]):
-        for a in range(len(my_list)-1, -1, -1):
-            print("{:d}".format(my_list[a]))
+def print_reversed_list_integer(my_list=[]):
+    for a in range(len(my_list)-1, -1, -1):
+        print("{:d}".format(my_list[a]))
