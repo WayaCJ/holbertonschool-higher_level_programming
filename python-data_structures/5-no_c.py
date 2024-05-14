a65f0ec3f998e7ca5fa99687acf99dfd358574f5
Carlos <sotocarlos1956@gmail.com>
2024-05-14 11:49:32 -0400

    deleting __name__

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def no_c(my_string):
-        result = ""
-        for char in my_string:
-            if char != 'c' and char != 'C':
-                result += char
-        return result
+def no_c(my_string):
+    result = ""
+    for char in my_string:
+       if char != 'c' and char != 'C':
+            result += char
+    return result
