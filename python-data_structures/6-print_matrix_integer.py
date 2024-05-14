59c5f2e5ffb5d1e1e37cd5711f90fe67c36cfd68
Carlos <sotocarlos1956@gmail.com>
2024-05-14 11:53:05 -0400

    deleting __name__

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
+    for m in range(len(matrix)):
+        for n in range(len(matrix[m])):
+            if n < len(matrix[m]) - 1:
+                print("{:d} ".format(matrix[m][n]), end="")
+            else:
+                print("{:d}".format(matrix[m][n]), end="")
         print()
