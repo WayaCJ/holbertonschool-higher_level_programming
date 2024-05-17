17aae7da047a06d0135f4eaf424ab42ab5dfbfe4
Carlos <sotocarlos1956@gmail.com>
2024-05-17 07:46:00 -0400

    1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,6 +3,8 @@
 
 
 class Square:
+   
+   
     """Class Square
         
         Attributes:
@@ -10,11 +12,9 @@ class Square:
 
         """
     def __init__(self, size):
+       
+       
         """Args:
         size: no type/value verification
         """
         self.__size = size
-    def size(self):
-        return self.__size
-    def set_size(self, value):
-        self.__size = value
