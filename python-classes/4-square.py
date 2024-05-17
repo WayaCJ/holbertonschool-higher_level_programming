3cf334c1ea4fd0c4e81f5269c86878b68b8f3063
Carlos <sotocarlos1956@gmail.com>
2024-05-17 11:57:54 -0400

    3,4

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module defines a class called Square
+"""
+
+
+class Square:
+    """class Square
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size: Private instance attribute defaults to zero.
+        Raises:
+            TypeError: must be int
+            ValueError: must be greater than zero
+        """
+
+        
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Calculates area
+            Returns: value of area
+        """
+        return self.__size * self.__size
