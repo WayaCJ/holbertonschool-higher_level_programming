c9cd0ece7be3f9e3e5ce719de5c798d0967364a8
Carlos <sotocarlos1956@gmail.com>
2024-05-17 12:05:34 -0400

    4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -26,4 +26,21 @@ class Square:
         """Calculates area
             Returns: value of area
         """
-        return self.__size * self.__size
+        return self.__size * 2
+    @property
+    def size(self):
+        """
+        Retrieve size
+        Return: value of area
+        """
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """
+        set size
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
