73fb3cd7de709f704155d1498effe8749e94e9dd
Carlos <sotocarlos1956@gmail.com>
2024-05-17 07:59:30 -0400

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class called Square
-    Raises:
-        size must be an integer, otherwise raise a TypeError
-        size must be greater than 0, otherwise raise a ValueError
+    
 """
 
 class Square:
@@ -11,7 +9,10 @@ class Square:
     def __init__(self, size=0):
         """
         Args:
-        size: Private instance attribute defaults to zero.
+            size: Private instance attribute defaults to zero.
+        Raises:
+            TypeError: must be int
+            ValueError: must be greater than zero
         """
         if type(size) != int:
             raise TypeError("size must be an integer")
