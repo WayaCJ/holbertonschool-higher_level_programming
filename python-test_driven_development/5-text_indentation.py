11cb038bd5c648d005e9779db22bc61b0a4872b1
Carlos <sotocarlos1956@gmail.com>
2024-05-21 11:21:55 -0400

    saving

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""This module creates a function that prints text
+"""
+
+
+def text_indentation(text):
+    """ Function that prints indented text.
+    Args:
+        text (str): text to print
+    """
+
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    flag = 0
+    newstring = ""
+    for i in text:
+        if flag == 1:
+            newstring = ""
+            flag = 0
+        if i not in "?:.":
+            newstring += i
+        else:
+            newstring += i
+            print(newstring.strip())
+            print()
+            flag = 1
+    if flag == 0 and '\n' not in newstring:
+        print(newstring.strip(), end="")
+    elif flag == 0:
+        print(newstring.strip())
