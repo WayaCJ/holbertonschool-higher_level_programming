8402bb0273004844a73e3e9df47752ffd12ce4fa
Carlos <sotocarlos1956@gmail.com>
2024-06-01 22:37:27 -0400

    saving

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -9,51 +9,54 @@ users = {
     "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
 }
 
+
 @app.route('/')
 def home():
-    """
-    welcome message.
-    """
-
+    """endpoint that returns a welcome message"""
     return "Welcome to the Flask API!"
 
-@app.route('/data', methods=['GET'])
-def get_data():
-    """
-    A JSON response containing a list of all usernames.
-    """
 
+@app.route('/data')
+def get_data():
+    """endpoint that return usernames"""
     return jsonify(list(users.keys()))
 
-@app.route('/status', methods=['GET'])
-def status():
-    """
-    A message indicating the API status.
-    """
+
+@app.route('/status')
+def get_status():
+    """endpoint that checks API status"""
 
     return "OK"
 
-@app.route('/users/<username>', methods=['GET'])
-def get_user(username):
-    """
-  The username to retrieve.
-    """
 
+@app.route('/users/<username>')
+def get_user(username):
+    """endpoint that details by username.
+    """
     user = users.get(username)
     if user:
         return jsonify(user)
-    return "User not found", 404
+    else:
+        return jsonify({"error": "User not found"}), 404
+
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
+    """endpoint to add a new user
     """
-    Accepts JSON data with user details and adds it to the users dictionary.
-    """
+    data = request.get_json()
+    if data:
+        username = data.get('username')
+        if not username:
+            return jsonify({"error": "Username not provided"}), 400
+        elif username in users:
+            return jsonify({"error": "Username already exists"}), 400
+        else:
+            users[username] = data
+            return jsonify({"message": "User added", "user": data}), 201
+    else:
+        return jsonify({"error": "Invalid JSON"}), 400
 
-    user_data = request.json
-    username = user_data['username']
-    users[username] = user_data
-    return jsonify({"message": "User added", "user": user_data})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
